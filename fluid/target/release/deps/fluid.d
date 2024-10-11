e737dcd76e00f53e76450debb5121c88bd98d7c1
Snowplou <snowplou01@gmail.com>
2024-10-11 15:54:34 -0700

    decided not to do grid simulation

--- fluid/target/release/deps/fluid.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jacob\Documents\VSC\Physcis Simulations\fluid\target\release\deps\fluid.exe: src/main.rs
-
-C:\Users\jacob\Documents\VSC\Physcis Simulations\fluid\target\release\deps\fluid.d: src/main.rs
-
-src/main.rs:
--- fluid/target/release/fluid.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jacob\Documents\VSC\Physcis\ Simulations\fluid\target\release\fluid.exe: C:\Users\jacob\Documents\VSC\Physcis\ Simulations\fluid\src\main.rs

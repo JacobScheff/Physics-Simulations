13225521ddda24fdf3f2888629f759a9af6ca5cf
Snowplou <snowplou01@gmail.com>
2024-10-17 17:49:17 -0700

    fixed time logging

--- cuda-fluid/main.cu
+++ cuda-fluid/main.cu
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <math.h>
+#include <chrono>
 
-// function to add the elements of two arrays
+// Kernel function to add the elements of two arrays
+__global__
 void add(int n, float *x, float *y)
 {
-  for (int i = 0; i < n; i++)
-      y[i] = x[i] + y[i];
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
 {
-  int N = 1<<20; // 1M elements
+  int N = 1<<20;
+  float *x, *y;
 
-  float *x = new float[N];
-  float *y = new float[N];
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
 
   // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
@@ -21,8 +27,17 @@ int main(void)
     y[i] = 2.0f;
   }
 
-  // Run kernel on 1M elements on the CPU
-  add(N, x, y);
+  // Get start time
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // Run kernel on 1M elements on the GPU
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  add<<<numBlocks, blockSize>>>(N, x, y);
+
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
@@ -30,9 +45,14 @@ int main(void)
     maxError = fmax(maxError, fabs(y[i]-3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
-  // Free memory
-  delete [] x;
-  delete [] y;
+  // Get end time
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<float> duration = end - start;
+  std::cout << "Time: " << duration.count() << "s" << std::endl;
 
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
   return 0;
 }

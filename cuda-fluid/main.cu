433a85d07937773e55401f352ee39d38afc68a7b
Snowplou <snowplou01@gmail.com>
2024-10-17 18:13:20 -0700

    added particle data

--- cuda-fluid/main.cu
+++ cuda-fluid/main.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <chrono>
+#include <array>
 
 // Kernel function to add the elements of two arrays
 __global__
@@ -14,36 +15,56 @@ void add(int n, float *x, float *y)
 
 int main(void)
 {
-  int N = 1<<20;
-  float *x, *y;
+  std::array<int, 2> const SCREEN_SIZE = {800, 400}; // The size of the screen
+  int const TIME_BETWEEN_FRAMES = 2;
+  std::array<int, 2> const GRID_SIZE = {80, 40}; // How many grid cells to divide the screen into
+
+  float const PARTICLE_RADIUS = 1.25; // The radius of the particles
+  int const PARTICLE_AMOUNT_X = 192; // The number of particles in the x direction
+  int const PARTICLE_AMOUNT_Y = 96; // The number of particles in the y direction
+  int const PARTICLE_AMOUNT = PARTICLE_AMOUNT_X * PARTICLE_AMOUNT_Y; // The total number of particles
+  float const PADDING = 50.0; // The padding around the screen
+
+  // Initialize data
+  float **positions, **velocities, **pressure_force, **viscosity_force;
+  float *densities, *radii;
 
   // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
+  cudaMallocManaged(&positions, PARTICLE_AMOUNT * sizeof(float*));
+  cudaMallocManaged(&velocities, PARTICLE_AMOUNT * sizeof(float*));
+  cudaMallocManaged(&pressure_force, PARTICLE_AMOUNT * sizeof(float*));
+  cudaMallocManaged(&viscosity_force, PARTICLE_AMOUNT * sizeof(float*));
+  cudaMallocManaged(&densities, PARTICLE_AMOUNT * sizeof(float));
+  cudaMallocManaged(&radii, PARTICLE_AMOUNT * sizeof(float));
 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+  // Initialize data
+  for (int i = 0; i < PARTICLE_AMOUNT; i++) {
+    positions[i] = new float[2];
+    velocities[i] = new float[2];
+    pressure_force[i] = new float[2];
+    viscosity_force[i] = new float[2];
+    positions[i][0] = (i + 0.5) * (SCREEN_SIZE[0] - 2.0 * PADDING) / PARTICLE_AMOUNT_X + PADDING;
+    positions[i][1] = (i + 0.5) * (SCREEN_SIZE[1] - 2.0 * PADDING) / PARTICLE_AMOUNT_Y + PADDING;
+    velocities[i][0] = 0.0;
+    velocities[i][1] = 0.0;
+    densities[i] = 0.0;
+    radii[i] = PARTICLE_RADIUS;
+    pressure_force[i][0] = 0.0;
+    pressure_force[i][1] = 0.0;
+    viscosity_force[i][0] = 0.0;
+    viscosity_force[i][1] = 0.0;
   }
 
   // Get start time
   auto start = std::chrono::high_resolution_clock::now();
 
-  // Run kernel on 1M elements on the GPU
-  int blockSize = 256;
-  int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<numBlocks, blockSize>>>(N, x, y);
-
+  // // // Run kernel on the GPU
+  // // int blockSize = 256;
+  // // int numBlocks = (N + blockSize - 1) / blockSize;
+  // // add<<<numBlocks, blockSize>>>(N, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
-
-  // Check for errors (all values should be 3.0f)
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
-  std::cout << "Max error: " << maxError << std::endl;
 
   // Get end time
   auto end = std::chrono::high_resolution_clock::now();
@@ -51,8 +72,14 @@ int main(void)
   std::cout << "Time: " << duration.count() << "s" << std::endl;
 
   // Free memory
-  cudaFree(x);
-  cudaFree(y);
+  cudaFree(positions);
+  cudaFree(velocities);
+  cudaFree(pressure_force);
+  cudaFree(viscosity_force);
+  cudaFree(densities);
+  cudaFree(radii);
+
+  std::cout << "Hello, World!" << std::endl;
   
   return 0;
 }

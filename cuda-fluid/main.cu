ad5e22d1fcf2ca5681bd64e24287c1b5a4f1795d
Snowplou <snowplou01@gmail.com>
2024-10-19 14:22:29 -0700

    fixed bugs

--- cuda-fluid/main.cu
+++ cuda-fluid/main.cu
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <math.h>
 #include <chrono>
-#include <array>
 #include <vector>
 
-std::array<int, 2> const SCREEN_SIZE = {800, 400}; // The size of the screen
-std::array<int, 2> const GRID_SIZE = {80, 40};     // How many grid cells to divide the screen into
+#define SCREEN_SIZE_X 1200
+#define SCREEN_SIZE_Y 600
+#define GRID_SIZE_X 80
+#define GRID_SIZE_Y 40
+int const SCREEN_SIZE_C[2] = {SCREEN_SIZE_X, SCREEN_SIZE_Y}; // The size of the screen
+int const GRID_SIZE_C[2] = {GRID_SIZE_X, GRID_SIZE_Y};     // How many grid cells to divide the screen into
 
 int const TIME_BETWEEN_FRAMES = 2;
 float const PARTICLE_RADIUS = 1.25;                                // The radius of the particles
@@ -23,43 +26,44 @@ float const VISCOSITY = 0.1; // The viscosity of the fluid
 float const DAMPENING = 0.95; // How much to slow down particles when they collide with the walls
 float const dt = 1.0 / 8.0; // The time step
 
-std::array<int, 2> const GRIDS_TO_CHECK = {int(RADIUS_OF_INFLUENCE / SCREEN_SIZE[0] * GRID_SIZE[0] + 1.0), int(RADIUS_OF_INFLUENCE / SCREEN_SIZE[1] * GRID_SIZE[1] + 1.0)}; // How many grid cells to check in each direction
+int const GRIDS_TO_CHECK[2] = {int(RADIUS_OF_INFLUENCE / SCREEN_SIZE_C[0] * GRID_SIZE_C[0] + 1.0), int(RADIUS_OF_INFLUENCE / SCREEN_SIZE_C[1] * GRID_SIZE_C[1] + 1.0)}; // How many grid cells to check in each direction
 
 // Grid functions
-__host__
-std::array<int, 2> pos_to_grid(float x, float y)
+__device__ __host__
+int* pos_to_grid(float x, float y)
 {
-  return {
-      (int)fmax(fmin(floor(x / SCREEN_SIZE[0] * GRID_SIZE[0]), GRID_SIZE[0] - 1), 0),
-      (int)fmax(fmin(floor(y / SCREEN_SIZE[1] * GRID_SIZE[1]), GRID_SIZE[1] - 1), 0)};
+  int grid[2] =  {
+      (int)fmax(fmin(floor(x / SCREEN_SIZE_X * GRID_SIZE_X), GRID_SIZE_X - 1), 0),
+      (int)fmax(fmin(floor(y / SCREEN_SIZE_Y * GRID_SIZE_Y), GRID_SIZE_Y - 1), 0)};
+  return grid;
 }
 
-__host__
+__device__ __host__
 int grid_to_index(int x, int y)
 {
-  return y * GRID_SIZE[0] + x;
+  return y * GRID_SIZE_X + x;
 }
 
 // Kernel function to calculate densities
-__global__ void calculate_densities(float **positions, float *densities, float *radii, int *particle_lookup, int *particle_counts, int particle_amount)
+__global__ void calculate_densities(float **positions, float *densities, float *radii, int *particle_lookup, int *particle_counts, int GRIDS_TO_CHECK_X, int GRIDS_TO_CHECK_Y, int particle_amount)
 {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   if (index >= PARTICLE_AMOUNT)
     return;
 
-  std::array<int, 2> grid = pos_to_grid(positions[index][0], positions[index][1]);
+  int* grid = pos_to_grid(positions[index][0], positions[index][1]);
   float density = 0.0;
 
-  for(int g = 0; g < (GRIDS_TO_CHECK[0] * 2 + 1) * (GRIDS_TO_CHECK[1] * 2 + 1); g++){
-    int gx = g / (GRIDS_TO_CHECK[1] * 2 + 1) - GRIDS_TO_CHECK[0];
-    int gy = g % (GRIDS_TO_CHECK[1] * 2 + 1) - GRIDS_TO_CHECK[1];
+  for(int g = 0; g < (GRIDS_TO_CHECK_X * 2 + 1) * (GRIDS_TO_CHECK_Y * 2 + 1); g++){
+    int gx = g / (GRIDS_TO_CHECK_Y * 2 + 1) - GRIDS_TO_CHECK_X;
+    int gy = g % (GRIDS_TO_CHECK_Y * 2 + 1) - GRIDS_TO_CHECK_Y;
 
-    if(grid[0] + gx < 0 || grid[0] + gx >= GRID_SIZE[0] || grid[1] + gy < 0 || grid[1] + gy >= GRID_SIZE[1]){
+    if(grid[0] + gx < 0 || grid[0] + gx >= GRID_SIZE_X || grid[1] + gy < 0 || grid[1] + gy >= GRID_SIZE_Y){
       continue;
     }
 
     int first_grid_index = grid_to_index(grid[0] + gx, grid[1] + gy);
-    if(first_grid_index < 0 || first_grid_index >= GRID_SIZE[0] * GRID_SIZE[1]){
+    if(first_grid_index < 0 || first_grid_index >= GRID_SIZE_X * GRID_SIZE_Y){
       continue;
     }
 
@@ -78,10 +82,10 @@ __global__ void calculate_densities(float **positions, float *densities, float *
 void sort(float **positions, float **velocities, float *radii, float *densities, float **pressure_force, float **viscosity_force, int *particle_lookup, int *particle_counts)
 {
   // Map all particles to their grid cell
-  std::vector<std::vector<std::vector<int>>> index_map(GRID_SIZE[0], std::vector<std::vector<int>>(GRID_SIZE[1], std::vector<int>()));
+  std::vector<std::vector<std::vector<int>>> index_map(GRID_SIZE_C[0], std::vector<std::vector<int>>(GRID_SIZE_C[1], std::vector<int>()));
   for (int i = 0; i < PARTICLE_AMOUNT; i++)
   {
-    std::array<int, 2> grid = pos_to_grid(positions[i][0], positions[i][1]);
+    int* grid = pos_to_grid(positions[i][0], positions[i][1]);
     index_map[grid[0]][grid[1]].push_back(i);
   }
 
@@ -94,11 +98,11 @@ void sort(float **positions, float **velocities, float *radii, float *densities,
   float **new_viscosity_force = new float *[PARTICLE_AMOUNT];
 
   // Iterate over all grid cells
-  for (int i = 0; i < GRID_SIZE[0]; i++)
+  for (int i = 0; i < GRID_SIZE_C[0]; i++)
   {
-    for (int j = 0; j < GRID_SIZE[1]; j++)
+    for (int j = 0; j < GRID_SIZE_C[1]; j++)
     {
-      int grid_index = i + j * GRID_SIZE[0];
+      int grid_index = i + j * GRID_SIZE_C[0];
       int index = -1;
 
       // Iterate over all particles in the grid cell
@@ -129,6 +133,13 @@ void sort(float **positions, float **velocities, float *radii, float *densities,
   densities = new_densities;
   pressure_force = new_pressure_force;
   viscosity_force = new_viscosity_force;
+
+  delete[] new_positions;
+  delete[] new_velocities;
+  delete[] new_radii;
+  delete[] new_densities;
+  delete[] new_pressure_force;
+  delete[] new_viscosity_force;
 }
 
 int main(void)
@@ -145,8 +156,8 @@ int main(void)
   cudaMallocManaged(&viscosity_force, PARTICLE_AMOUNT * sizeof(float *));
   cudaMallocManaged(&densities, PARTICLE_AMOUNT * sizeof(float));
   cudaMallocManaged(&radii, PARTICLE_AMOUNT * sizeof(float));
-  cudaMallocManaged(&particle_lookup, GRID_SIZE[0] * GRID_SIZE[1] * sizeof(int));
-  cudaMallocManaged(&particle_counts, GRID_SIZE[0] * GRID_SIZE[1] * sizeof(int));
+  cudaMallocManaged(&particle_lookup, GRID_SIZE_C[0] * GRID_SIZE_C[1] * sizeof(int));
+  cudaMallocManaged(&particle_counts, GRID_SIZE_C[0] * GRID_SIZE_C[1] * sizeof(int));
 
   // Initialize data
   for (int i = 0; i < PARTICLE_AMOUNT; i++)
@@ -155,8 +166,8 @@ int main(void)
     velocities[i] = new float[2];
     pressure_force[i] = new float[2];
     viscosity_force[i] = new float[2];
-    positions[i][0] = (i + 0.5) * (SCREEN_SIZE[0] - 2.0 * PADDING) / PARTICLE_AMOUNT_X + PADDING;
-    positions[i][1] = (i + 0.5) * (SCREEN_SIZE[1] - 2.0 * PADDING) / PARTICLE_AMOUNT_Y + PADDING;
+    positions[i][0] = (i + 0.5) * (SCREEN_SIZE_C[0] - 2.0 * PADDING) / PARTICLE_AMOUNT_X + PADDING;
+    positions[i][1] = (i + 0.5) * (SCREEN_SIZE_C[1] - 2.0 * PADDING) / PARTICLE_AMOUNT_Y + PADDING;
     velocities[i][0] = 0.0;
     velocities[i][1] = 0.0;
     densities[i] = 0.0;
@@ -166,7 +177,7 @@ int main(void)
     viscosity_force[i][0] = 0.0;
     viscosity_force[i][1] = 0.0;
 
-    if (i < GRID_SIZE[0] * GRID_SIZE[1])
+    if (i < GRID_SIZE_C[0] * GRID_SIZE_C[1])
     {
       particle_lookup[i] = -1;
       particle_counts[i] = 0;
@@ -184,7 +195,7 @@ int main(void)
   int numBlocks = (PARTICLE_AMOUNT + blockSize - 1) / blockSize;
 
   // Calculate densities
-  calculate_densities<<<numBlocks, blockSize>>>(positions, densities, radii, particle_lookup, particle_counts, PARTICLE_AMOUNT);
+  calculate_densities<<<numBlocks, blockSize>>>(positions, densities, radii, particle_lookup, particle_counts, GRIDS_TO_CHECK[0], GRIDS_TO_CHECK[1], PARTICLE_AMOUNT);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
